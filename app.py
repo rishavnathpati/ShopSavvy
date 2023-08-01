723827fe5e3f380d2276aec2dd4b12e3257776c5
Rishav Nath Pati <patirishavnath@gmail.com>
2023-08-01 10:08:30 +0530

    Complete about page

--- app.py
+++ app.py
@@ -33,6 +33,14 @@ def upload_image(image_path):
 def home():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
 
 # @app.route('/search', methods=['POST'])
 # def upload_file():
