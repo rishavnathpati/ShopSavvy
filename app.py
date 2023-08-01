8525e247e2ece5675f28354b6ab8cef323e58c73
Rishav Nath Pati <patirishavnath@gmail.com>
2023-08-01 10:26:22 +0530

    Added custom query entry

--- app.py
+++ app.py
@@ -33,91 +33,72 @@ def upload_image(image_path):
 def home():
     return render_template("index.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
+
 
 @app.route("/index")
 def index():
     return render_template("index.html")
 
 
-# @app.route('/search', methods=['POST'])
-# def upload_file():
-#     if 'file' not in request.files:
-#         return redirect(request.url)
-#     file = request.files['file']
-#     if file.filename == '':
-#         return redirect(request.url)
-#     if file:
-#         filename = secure_filename(file.filename)
-#         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-#         file.save(file_path)
-#         image_url = upload_image(file_path)
-#         if image_url is not None:
-#             params = {
-#                 "q": "shopping",
-#                 "engine": "google_reverse_image",
-#                 "image_url": image_url,
-#                 "location_requested": "Delhi, India",
-#                 "google_domain": "google.com",
-#                 "hl": "en",
-#                 "gl": "in",
-#                 "start": 0,
-#                 "device": "desktop",
-#                 "api_key": "0f27d0f205a1366cb097a52480fc6aaa37fb48b457056337d6354bb6cc9729ea"
-#             }
-#             search = GoogleSearch(params)
-#             results = search.get_dict()
-#             visual_matches = results["image_results"]
-#             return render_template('results.html', results=visual_matches)
-#         else:
-#             return "Error uploading image"
-#     return redirect(request.url)
-
-
-@app.route('/search', methods=['POST'])
+@app.route("/search", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
+    query = request.form.get(
+        "query", ""
+    )  
+    if "file" not in request.files:
         return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
+    file = request.files["file"]
+    if file.filename == "":
         return redirect(request.url)
     if file:
         filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(file_path)
         image_url = upload_image(file_path)
         if image_url is not None:
             params = {
-                "q": "shopping,india",
+                "q": query+"Shopping, India",  # use the custom query here
                 "engine": "google_lens",
                 "url": image_url,
-                "api_key": "0f27d0f205a1366cb097a52480fc6aaa37fb48b457056337d6354bb6cc9729ea"
+                "api_key": "0f27d0f205a1366cb097a52480fc6aaa37fb48b457056337d6354bb6cc9729ea",
             }
+
             search = GoogleSearch(params)
             results = search.get_dict()
             visual_matches = results["visual_matches"]
-            
+
             # Split results into ones with price and ones without
-            without_price = [result for result in visual_matches if 'price' not in result or 'value' not in result['price']]
-            with_price = [result for result in visual_matches if 'price' in result and 'value' in result['price']]
+            without_price = [
+                result
+                for result in visual_matches
+                if "price" not in result or "value" not in result["price"]
+            ]
+            with_price = [
+                result
+                for result in visual_matches
+                if "price" in result and "value" in result["price"]
+            ]
 
             # Convert price to float
             for result in with_price:
-                result['price']['extracted_value'] = float(result['price']['extracted_value'])
+                result["price"]["extracted_value"] = float(
+                    result["price"]["extracted_value"]
+                )
 
             # Sort ones with price by price (lowest to highest)
-            with_price = sorted(with_price, key=lambda k: k['price']['extracted_value'])
+            with_price = sorted(with_price, key=lambda k: k["price"]["extracted_value"])
 
             # Concatenate the ones without price at the start and the top 20 ones with price
             visual_matches = without_price + with_price[:20]
 
-            return render_template('results.html', results=visual_matches)
+            return render_template("results.html", results=visual_matches)
         else:
             return "Error uploading image"
     return redirect(request.url)
-
 
 
 if __name__ == "__main__":
