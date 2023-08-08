79000e411cd399fa81423d52b871aad5c8a0b3ac
Rishav Nath Pati <patirishavnath@gmail.com>
2023-08-08 11:14:39 +0530

    Logout working

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def profile():
 @app.route("/logout")
 def logout():
     session.pop("username", None)
-    return redirect(url_for("index.html"))
+    return redirect(url_for("login"))
 
 
 @app.route("/")
