ca66fed557efc19e30c7416b9da4d7bd3c0985e6
Rishav Nath Pati <patirishavnath@gmail.com>
2023-08-08 10:20:31 +0530

    login code

--- app.py
+++ app.py
@@ -1,15 +1,80 @@
 import os
 import subprocess
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.utils import secure_filename
 from serpapi import GoogleSearch
-from flask import flash
-
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 UPLOAD_FOLDER = "uploads"
-
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["SECRET_KEY"] = "my-secret-key"  
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if User.query.filter_by(username=username).first():
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        user = User(username=username, password_hash=generate_password_hash(password))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username).first()
+        if user is None or not check_password_hash(user.password_hash, password):
+            flash("Please check your login details and try again.")
+            return redirect(url_for("login"))
+        session["username"] = username
+        return redirect(url_for("profile"))
+    return render_template("login.html")
+
+
+@app.route("/profile")
+def profile():
+    if "username" not in session:
+        return redirect(url_for("login"))
+    return render_template("profile.html", username=session["username"])
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 
 def upload_image(image_path):
@@ -40,47 +105,10 @@ def upload_image(image_path):
         return None
 
 
-@app.route("/")
-def home():
-    """
-    Handles the root ("/") route.
-
-    Returns:
-    str: The HTML content of the home page.
-    """
-    return render_template("index.html")
-
-
-@app.route("/about")
-def about():
-    """
-    Handles the "/about" route.
-
-    Returns:
-    str: The HTML content of the about page.
-    """
-    return render_template("about.html")
-
-
-@app.route("/index")
-def index():
-    """
-    Handles the "/index" route.
-
-    Returns:
-    str: The HTML content of the index page.
-    """
-    return render_template("index.html")
-
-
 @app.route("/search", methods=["POST"])
 def upload_file():
-    """
-    Handles the "/search" route, which accepts POST requests. Uploads a file and performs a Google Lens search.
-
-    Returns:
-    str: The HTML content of the results page, or a redirect to the previous page if an error occurred.
-    """
+    if "username" not in session:
+        return redirect(url_for("login"))
     query = request.form.get("query", "")
     if "file" not in request.files:
         flash("No file part in the request")
@@ -106,7 +134,6 @@ def upload_file():
             results = search.get_dict()
             visual_matches = results["visual_matches"]
 
-            # Split results into ones with price and ones without
             without_price = [
                 result
                 for result in visual_matches
@@ -118,18 +145,13 @@ def upload_file():
                 if "price" in result and "value" in result["price"]
             ]
 
-            # Convert price to float
             for result in with_price:
                 result["price"]["extracted_value"] = float(
                     result["price"]["extracted_value"]
                 )
 
-            # Sort ones with price by price (lowest to highest)
             with_price = sorted(with_price, key=lambda k: k["price"]["extracted_value"])
-
-            # Concatenate the ones without price at the start and the top 20 ones with price
             visual_matches = without_price + with_price[:20]
-
             return render_template("results.html", results=visual_matches)
         else:
             return "Error uploading image"
@@ -137,4 +159,7 @@ def upload_file():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
